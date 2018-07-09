376b1da066aa9311f952fc523f2fdda9f15b569b
Suthep <suthepsvp@hotmail.com>
2018-07-09 09:36:29 +0700

    Add POST /todos route and refractor mongoose

---
+++ server/models/todo.js
@@ -0,0 +1,29 @@
+var mongoose = require('mongoose');
+
+var Todo = mongoose.model('Todo', {
+  text: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+  },
+  completed: {
+    type: Boolean,
+    default: false
+  },
+  completedAt: {
+    type: Number,
+    default: null
+  }
+});
+
+var newTodo = new Todo({
+  text: 'Cook dinner'
+});
+var otherTodo = new Todo({
+  text: 'Feed cat',
+  completed: false,
+  completedAt: 123
+});
+
+module.exports = {Todo};
---
+++ server/server.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  });
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+
+app.listen(2500, () => {
+  console.log('Started on port 2500');
+});
+
+
+// newTodo.save().then((doc) => {
+//   console.log('Saved todo ', doc)
+// }, (e) => {
+//   console.log('Unable to save to do');
+// });
